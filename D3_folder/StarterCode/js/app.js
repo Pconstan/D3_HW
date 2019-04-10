ab6f512600ddea9056f8b5d4d4208cbcda919e9c
Pconstan <pconstantelos@gmail.com>
2019-04-10 16:47:18 -0500

    New repo for hw

---
+++ D3_folder/StarterCode/js/app.js
@@ -0,0 +1,35 @@
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 60,
+  left: 50
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+
+
+var svg = d3
+  .select("body")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Step 3:
+// Import data from the donuts.csv file
+// =================================
+d3.csv("data.csv", function(error, donutData) {
+  if (error) throw error;
+
+
+
+
+  
+});
